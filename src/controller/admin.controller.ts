51d0d8caf7af7e2b28ddc019d67bfaedccc65c3a
Farooq Ajibade <timiajibade24@gmail.com>
2023-04-05 12:52:59 +0100

    quick fixes in controllers and redirect url in verify email updated

--- src/controller/admin.controller.ts
+++ src/controller/admin.controller.ts
@@ -100,6 +100,7 @@ export const getAllDrivers = async (
     });
   }
 };
+
 // Get one Driver//
 export const getOneDriver = async (
   req: Request,
@@ -325,25 +326,4 @@ export const tripHistory = async (req: Request, res: Response) => {
   }
 };
 
-export const tripHistoryByPassenger = async (req: Request, res: Response) => {
-  try {
-    const userId = req.userId;
-    const user = await User.findById({ _id: userId });
-    if (user) {
-      const { name } = user;
-      const tripsByUser = await Trip.find({ passenger: name });
-      console.log(tripsByUser);
-      if (tripsByUser) {
-        res.status(200).json({ status: 'success', data: tripsByUser });
-      } else {
-        res
-          .status(404)
-          .json({ status: 'failed', message: 'No trips created by user' });
-      }
-    }
-  } catch (err: any) {
-    res
-      .status(400)
-      .json({ message: 'Internal server error', error: err.message });
-  }
-};
+
